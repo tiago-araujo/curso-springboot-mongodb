020c55713a79d2eb0e6caa40866b399f6d09ce30
Tiago Araújo <tiaguzaum_@hotmail.com>
2022-05-13 11:26:48 -0400

    first commit

---
+++ src/main/java/com/educacao/cursospringmongo/CursospringmongoApplication.java
@@ -0,0 +1,13 @@
+package com.educacao.cursospringmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/educacao/cursospringmongo/CursospringmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educacao.cursospringmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
